57113da02e9327732ae18ef957fb871372e65e16
Ryan Rymarczyk <rymarczy@gmail.com>
2024-08-30 16:36:57 -0400

    ignore schema ordinal column

--- src/cubic_loader/qlik/ods_qlik.py
+++ src/cubic_loader/qlik/ods_qlik.py
@@ -38,7 +38,6 @@ from cubic_loader.utils.logger import ProcessLogger
 
 DFM_COLUMN_SCHEMA = pl.Schema(
     {
-        "ordinal": pl.Int64(),
         "name": pl.String(),
         "type": pl.String(),
         "length": pl.Int64(),
