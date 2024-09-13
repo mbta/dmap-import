8e794eada02e93fb8fe369a3d11b0ed6fea05aea
Ryan Rymarczyk <rymarczy@gmail.com>
2024-09-13 15:51:00 -0400

    skip payment_summary

--- src/cubic_loader/qlik/ods_tables.py
+++ src/cubic_loader/qlik/ods_tables.py
@@ -14,7 +14,7 @@ CUBIC_ODS_TABLES = [
     "EDW.DEVICE_DIMENSION",
     "EDW.USE_TRANSACTION",
     # comp b
-    "EDW.PAYMENT_SUMMARY",
+    # "EDW.PAYMENT_SUMMARY", Table has NULL values in PrimaryKey columns
     "EDW.TXN_CHANNEL_MAP",
     "EDW.CCH_AFC_TRANSACTION",
     "EDW.PATRON_TRIP",
