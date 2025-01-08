2638e873b40585a398e91ddff923ba474b6eeaf4
Ryan Rymarczyk <rymarczy@gmail.com>
2025-01-08 11:55:41 -0500

    also drop tables

--- src/alembic/versions/007_96837b10c106_reset_qlik_null_key_tables.py
+++ src/alembic/versions/007_96837b10c106_reset_qlik_null_key_tables.py
@@ -9,6 +9,8 @@ Create Date: 2021-01-07 19:42:12.287594
 import os
 from typing import Sequence, Union
 
+from alembic import op
+
 from cubic_loader.utils.aws import s3_delete_object
 from cubic_loader.utils.remote_locations import ODS_STATUS
 
@@ -24,6 +26,10 @@ def upgrade() -> None:
     # This will force a reset/re-load for these tables in dmap-import DB
     s3_delete_object(os.path.join(ODS_STATUS, "EDW.PAYMENT_SUMMARY.json"))
     s3_delete_object(os.path.join(ODS_STATUS, "EDW.MEMBER_DIMENSION.json"))
+    op.drop_table("edw_payment_summary", schema="ods")
+    op.drop_table("edw_payment_summary_history", schema="ods")
+    op.drop_table("edw_member_dimension", schema="ods")
+    op.drop_table("edw_member_dimension_history", schema="ods")
 
 
 def downgrade() -> None:
