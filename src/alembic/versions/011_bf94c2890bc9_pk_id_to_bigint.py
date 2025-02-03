e088771d498f33ce65a2199dde51ca800cf6929a
Ryan Rymarczyk <rymarczy@gmail.com>
2025-02-03 10:47:08 -0500

    fix format

--- src/alembic/versions/011_bf94c2890bc9_pk_id_to_bigint.py
+++ src/alembic/versions/011_bf94c2890bc9_pk_id_to_bigint.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "bf94c2890bc9"
-down_revision: Union[str, None] = 'ab250e2a0b0d'
+down_revision: Union[str, None] = "ab250e2a0b0d"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
