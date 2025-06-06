6582afc27eb07b671c2177e5ebc1ea6160a19e1d
Ryan Rymarczyk <rymarczy@gmail.com>
2025-06-06 09:17:33 -0400

    new views

--- src/cubic_loader/qlik/sql_strings/mat_views.py
+++ src/cubic_loader/qlik/sql_strings/mat_views.py
@@ -42,3 +42,66 @@ COMP_B_ADDENDUM = """
         )
     ;
 """
+
+LATE_TAP_ADJUSTMENT = """
+    DROP MATERIALIZED VIEW IF EXISTS ods.late_tap_adjustment;
+    CREATE MATERIALIZED VIEW ods.late_tap_adjustment AS
+    SELECT
+        s.settlement_day_key
+        ,s.operating_day_key
+        ,u.patron_trip_id
+        ,u.trip_price_count
+        ,tp.is_reversal
+        ,u.token_id
+        ,s.purse_load_id
+        ,-tp.stored_value::real / 100 AS uncollectible_amount
+        ,tp.transaction_dtm
+        ,u.transit_account_id
+        ,tm.travel_mode_name
+    FROM
+        ods.edw_sale_transaction s
+    JOIN
+        ods.edw_trip_payment tp
+        ON
+            tp.purse_load_id = s.purse_load_id
+    JOIN
+        ods.edw_patron_trip t
+        ON
+            t.patron_trip_id = tp.patron_trip_id
+    JOIN
+        ods.edw_use_transaction u
+        ON
+            u.patron_trip_id = tp.patron_trip_id
+            AND u.trip_price_count = tp.trip_price_count
+    LEFT JOIN
+        ods.edw_transaction_history en
+        ON
+            en.dw_transaction_id = t.dw_entry_txn_id
+    LEFT JOIN
+        ods.edw_travel_mode_dimension tm
+        ON
+            tm.travel_mode_id = en.travel_mode_id
+    WHERE
+        s.sale_type_key = 26
+        AND u.value_changed <> 0
+        AND
+        (
+            (
+                tp.je_is_fare_adjustment = 0
+                AND u.ride_type_key <> 24
+                AND tp.is_reversal = 1
+                AND u.fare_due < 0
+            )
+            OR
+            (
+                tp.je_is_fare_adjustment = 0
+                AND u.ride_type_key <> 24
+                AND tp.is_reversal = 0
+                AND u.fare_due > 0
+            )
+            OR (tp.je_is_fare_adjustment = 1 AND u.ride_type_key = 24)
+        )
+    ORDER BY
+        s.operating_day_key desc
+        ,s.settlement_day_key desc
+"""
--- src/cubic_loader/qlik/sql_strings/views.py
+++ src/cubic_loader/qlik/sql_strings/views.py
@@ -94,6 +94,47 @@ COMP_B_VIEW = """
 """
 
 
+COMP_C_VIEW = """
+    DROP VIEW IF EXISTS ods.wc700_comp_c;
+    CREATE VIEW ods.wc700_comp_c
+    AS
+    SELECT
+        t.settlement_day_key
+        ,t.operating_day_key
+        ,rc.rider_class_name
+        ,fp.fare_prod_name
+        ,t.service_type_id
+        ,t.fare_rule_description
+        ,t.recovery_txn_type
+        ,sum(t.minimum_fare_charge) / 100 AS recovery_calculation_amount
+    FROM
+        ods.edw_farerev_recovery_txn t
+    LEFT JOIN
+        ods.edw_rider_class_dimension rc
+        ON
+            rc.rider_class_id = t.rider_class_id
+    LEFT JOIN
+        ods.edw_fare_product_dimension fp
+        ON
+            fp.fare_prod_key = t.fare_prod_key
+    WHERE
+        fp.monetary_inst_type_id = 2
+        AND t.minimum_fare_charge IS NOT NULL
+    GROUP BY
+        t.operating_day_key
+        ,t.settlement_day_key
+        ,t.service_type_id
+        ,rc.rider_class_name
+        ,fp.fare_prod_name
+        ,t.fare_rule_description
+        ,t.recovery_txn_type
+    ORDER BY
+        operating_day_key desc
+        ,settlement_day_key desc
+    ;
+"""
+
+
 COMP_D_VIEW = """
     DROP VIEW IF EXISTS ods.wc700_comp_d;
     CREATE OR REPLACE VIEW ods.wc700_comp_d 
