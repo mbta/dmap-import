34b754594fdb942f36a0620aa9eba6cd79b85688
Ryan Rymarczyk <rymarczy@gmail.com>
2025-07-03 10:06:02 -0400

    feat: add WC321 and WC700 COMP C views
    This change adds WC321 and WC700 COMP C views to dmap-import DB.
    
    SQL statement for COMP C was previously added, but never deployed. This change adds both SQL statement and deployment for WC321.

---
+++ src/alembic/versions/016_6ce4db2752fb_wc231_compC.py
@@ -0,0 +1,36 @@
+"""add WC231 and WC700_COMP_C views
+
+Revision ID: 6ce4db2752fb
+Revises: 4b8117e23c8e
+Create Date: 2025-07-03 10:02:44.209868
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+from cubic_loader.utils.postgres import DatabaseManager
+from cubic_loader.qlik.sql_strings.views import WC321_CLEARING_HOUSE
+from cubic_loader.qlik.sql_strings.views import COMP_C_VIEW
+
+# revision identifiers, used by Alembic.
+revision: str = "6ce4db2752fb"
+down_revision: Union[str, None] = "4b8117e23c8e"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    db = DatabaseManager()
+    schema_check_query = "SELECT COUNT(*) from information_schema.tables WHERE table_schema = 'ods';"
+    if db.select(schema_check_query)["count"] == 0:
+        return
+
+    op.execute(WC321_CLEARING_HOUSE)
+    op.execute(COMP_C_VIEW)
+
+
+def downgrade() -> None:
+    op.execute("DROP VIEW IF EXISTS ods.wc700_comp_c;")
+    op.execute("DROP VIEW IF EXISTS ods.wc_321_clearing_house;")
