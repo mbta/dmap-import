156ff2c56cdb42ac6b9bb5e802ff0548938f40f3
Ted Alexander <talexander3@mbta.com>
2025-09-05 13:39:05 -0400

    Formatting

--- src/alembic/versions/020_aa29a832d93f_add_citation_dataset.py
+++ src/alembic/versions/020_aa29a832d93f_add_citation_dataset.py
@@ -5,6 +5,7 @@ Revises: 92ab6c167c5b
 Create Date: 2025-09-05 12:00:12.669250
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'aa29a832d93f'
-down_revision: Union[str, None] = '92ab6c167c5b'
+revision: str = "aa29a832d93f"
+down_revision: Union[str, None] = "92ab6c167c5b"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -34,7 +35,7 @@ def upgrade() -> None:
         sa.Column("citation_dtm", sa.DateTime(), nullable=True),
         sa.Column("inspector_id", sa.String(), nullable=True, index=True),
         sa.Column("citation_reason_name", sa.String(), nullable=True),
-        sa.Column("citation_amount", sa.String(), nullable=True, index=True), # maybe: use sa.Float()?
+        sa.Column("citation_amount", sa.String(), nullable=True, index=True),  # maybe: use sa.Float()?
         sa.Column("direction_id", sa.String(), nullable=True, index=True),
         sa.Column("block_number", sa.String(), nullable=True),
         sa.Column("recommendation", sa.String(), nullable=True),
@@ -48,6 +49,7 @@ def upgrade() -> None:
         sa.Column("_exported_dtm", sa.DateTime(), nullable=True),
         sa.PrimaryKeyConstraint("pk_id"),
     )
+
 
 def downgrade() -> None:
     op.drop_table("citation")
