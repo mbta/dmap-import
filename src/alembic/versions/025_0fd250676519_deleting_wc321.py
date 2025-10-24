d84a379cc43a65efa4145f6d43f3746a117fe17d
Ted Alexander <talexander3@mbta.com>
2025-10-24 15:22:07 -0400

    Deleting ods.wc_321_clearing_house (which will already have been replaced)

---
+++ src/alembic/versions/025_0fd250676519_deleting_wc321.py
@@ -0,0 +1,26 @@
+"""deleting WC321
+
+Revision ID: 0fd250676519
+Revises: 7ac3ef57d137
+Create Date: 2025-10-24 15:16:48.396353
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+from cubic_loader.utils.postgres import DatabaseManager
+from cubic_loader.qlik.sql_strings.views import WC321_CLEARING_HOUSE
+
+# revision identifiers, used by Alembic.
+revision: str = '0fd250676519'
+down_revision: Union[str, None] = 'd09f890f2828'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+def upgrade() -> None:
+    op.execute("DROP VIEW IF EXISTS ods.wc_321_clearing_house;")
+
+
+def downgrade() -> None:
+    op.execute(WC321_CLEARING_HOUSE)
