f3b0045c7652f807e982e49836c73b858a939a9f
Ted Alexander <82063375+ealexa05@users.noreply.github.com>
2025-12-30 17:39:14 -0500

    fix: prevent loading old data when Cubic regenerates use_transaction_… (#96)
    * fix: prevent loading old data when Cubic regenerates use_transaction_location

--- src/cubic_loader/dmap/dmap_api.py
+++ src/cubic_loader/dmap/dmap_api.py
@@ -98,7 +98,7 @@ def download_from_url(url: str, local_path: str) -> bool:
 
 
 # pylint: disable=too-many-locals
-def get_api_results(url: str, db_manager: DatabaseManager) -> List[ApiResult]:
+def get_api_results(url: str, db_manager: DatabaseManager) -> List[ApiResult]:  # pylint: disable=R0912
     """
     Execute GET request against CUBIC API URL using last_updated param to
     filter results based on last_updated timestamp from ApiMetadata DB table
@@ -137,7 +137,14 @@ def get_api_results(url: str, db_manager: DatabaseManager) -> List[ApiResult]:
     # last_updated if last_update_dt available from ApiMetadata table
     # apikey based on contents of url endpoint string
     headers = {"apikey": apikey_from_environment(url)}
-    params = {"limit": "100"}
+
+    # Hotfix for UseTransactionalLocation dataset
+    if url.endswith("use_transaction_location"):
+        min_start_date = "2025-12-01"
+        params = {"limit": "100", "start_date": min_start_date}
+    else:
+        params = {"limit": "100"}
+
     if db_result:
         last_updated_dt: datetime.datetime = db_result[0]["last_updated"]
         params["last_updated"] = (last_updated_dt.date() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
