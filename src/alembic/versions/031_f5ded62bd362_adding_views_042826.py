5980236a9fa03f175ac44ec5f40248ddf74e3d99
Ted Alexander <talexander3@mbta.com>
2026-04-28 13:14:31 -0400

    Formatting, because Alembic disagrees with Black

--- src/alembic/versions/031_f5ded62bd362_adding_views_042826.py
+++ src/alembic/versions/031_f5ded62bd362_adding_views_042826.py
@@ -5,6 +5,7 @@ Revises: 960c57e8e70b
 Create Date: 2026-04-28 12:56:14.621995
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -13,8 +14,8 @@ from cubic_loader.utils.postgres import DatabaseManager
 from cubic_loader.qlik.sql_strings.views import WC232_PATRON_ORDER_VIEW, WC232_REFUND_SALE_VIEW, WC232_USE_VIEW
 
 # revision identifiers, used by Alembic.
-revision: str = 'f5ded62bd362'
-down_revision: Union[str, None] = '960c57e8e70b'
+revision: str = "f5ded62bd362"
+down_revision: Union[str, None] = "960c57e8e70b"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
