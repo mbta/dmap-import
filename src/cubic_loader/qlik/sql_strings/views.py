422973b110766f9da0dc7acd74f77267ad3d933d
Ted Alexander <talexander3@mbta.com>
2026-04-28 13:09:24 -0400

    Adding WC232 views

--- src/cubic_loader/qlik/sql_strings/views.py
+++ src/cubic_loader/qlik/sql_strings/views.py
@@ -1377,3 +1377,152 @@ END  <>  'Refund Issue Media'
      )
     );
 """
+
+WC232_PATRON_ORDER_VIEW = """
+CREATE OR REPLACE VIEW ods.wc232_patron_order AS (SELECT
+  eupo.POSTING_DAY_KEY,
+  eupo.READY_FOR_SETTLEMENT_DTM,
+  eupo.ORDER_ORIGIN,
+  eupo.TRANSACTION_DTM,
+  eupo.ORDER_NBR,
+  eupo.LINE_ITEM_NBR,
+  eupo.ORDER_STATUS,
+  efpd.FARE_PROD_NAME,
+  pt.PAYMENT_TYPE_NAME,
+  COALESCE(eupo.DEPOSIT_VALUE,0)/100 AS deposit_value,
+  COALESCE(eupo.PASS_COST,0)/100 AS pass_cost,
+  COALESCE(eupo.TRANSIT_VALUE,0)/100 AS transit_value,
+  COALESCE(eupo.BENEFIT_VALUE,0)/100 AS benefit_value,
+  COALESCE(eupo.ADMINISTRATIVE_FEE,0)/100 AS administrative_fee,
+  COALESCE(eupo.REPLACEMENT_FEE,0)/100 AS replacement_fee,
+  COALESCE(eupo.SHIPPING_FEE,0)/100 AS shipping_fee,
+  COALESCE(eupo.REFUND_FEE,0)/100 AS refund_fee,
+  eupo.TRANSIT_ACCOUNT_ID,
+  eupo.SERIAL_NBR,
+  eod.OPERATOR_NAME,
+  erd.REASON_CODE,
+  erd.REASON_NAME,
+  eupo.PASS_ID,
+  eupo.SETTLEMENT_STATE,
+  COALESCE(eupo.ONE_ACCOUNT_VALUE,0)/100 AS one_account_value,
+  COALESCE(eupo.DISCOUNT_AMOUNT/100,0) AS discount_amount,
+  eptd.PURSE_NAME,
+  COALESCE(eupo.RESTRICTED_PURSE_VALUE,0)/100 AS restricted_purse_value,
+  eupo.RETRIEVAL_REF_NBR,
+  COALESCE(eupo.REFUNDABLE_PURSE_VALUE,0)/100 AS refundable_purse_value,
+  COALESCE(eupo.PREPAID_BANKCARD_VALUE/100,0) AS prepaid_bankcard_value,
+  eupo.PAYMENT_TRANSIT_ACCOUNT_ID
+FROM ods.edw_unsettled_patron_order eupo
+	LEFT JOIN ods.edw_reason_dimension erd ON eupo.reason_key = erd.reason_key
+	LEFT JOIN ods.edw_operator_dimension eod ON eupo.operator_key = eod.operator_key
+	LEFT JOIN ods.edw_fare_product_dimension efpd ON eupo.fare_prod_key = efpd.fare_prod_key
+	LEFT JOIN ods.edw_purse_type_dimension eptd ON eupo.purse_sku = eptd.purse_sku
+	LEFT JOIN ods.edw_payment_type_dimension pt ON eupo.payment_type_key = pt.payment_type_id
+);
+"""
+
+WC232_REFUND_SALE_VIEW = """
+CREATE OR REPLACE VIEW ods.wc232_refund_sale AS
+(WITH UNSETTLED_REFUND_SALE AS (
+SELECT s.settlement_state,
+       s.posting_day_key,
+       s.ready_for_settlement_dtm,
+       op.operator_name,
+       s.transaction_dtm,
+       s.transaction_id,
+       s.device_id,
+       pt.payment_type_name,
+       fp.fare_prod_name,
+       s.deposit_value,
+       s.pass_cost,
+       s.transit_value,
+       s.benefit_value,
+       s.one_account_value,
+       s.orig_sale_amt_paid_by_bv,
+       s.orig_sale_amt_paid_by_non_bv,
+       s.order_nbr,
+       s.transit_account_id,
+       s.serial_nbr,
+       s.oa_account_id,
+       r.reason_code,
+       r.reason_name,
+       s.pass_id,
+       s.dw_transaction_id,
+       s.operator_id,
+       s.fare_prod_key,
+       s.payment_type_key,
+       s.reason_key,
+       s.booking_prepaid_value,
+       s.purse_sku,
+       prstd.purse_name,
+       s.restricted_purse_value,
+       s.prepaid_bankcard_value,
+       s.refund_fee,
+       s.retrieval_ref_nbr,
+       s.payment_transit_account_id
+  FROM ods.edw_unsettled_sale s
+       LEFT JOIN ods.edw_operator_dimension op ON op.operator_id = s.operator_id
+       LEFT JOIN ods.edw_fare_product_dimension fp ON fp.fare_prod_key = s.fare_prod_key
+       LEFT JOIN ods.edw_payment_type_dimension pt ON pt.payment_type_key = s.payment_type_key
+       LEFT JOIN ods.edw_reason_dimension r ON r.reason_key = s.reason_key
+       LEFT JOIN ods.edw_purse_type_dimension prstd ON prstd.purse_sku = s.purse_sku
+ WHERE s.sale_type_key = 24)
+SELECT
+  UNSETTLED_REFUND_SALE.SETTLEMENT_STATE,
+  UNSETTLED_REFUND_SALE.POSTING_DAY_KEY,
+  UNSETTLED_REFUND_SALE.READY_FOR_SETTLEMENT_DTM,
+  UNSETTLED_REFUND_SALE.OPERATOR_NAME,
+  UNSETTLED_REFUND_SALE.TRANSACTION_DTM,
+  UNSETTLED_REFUND_SALE.TRANSACTION_ID,
+  UNSETTLED_REFUND_SALE.DEVICE_ID,
+  UNSETTLED_REFUND_SALE.PAYMENT_TYPE_NAME,
+  UNSETTLED_REFUND_SALE.FARE_PROD_NAME,
+  COALESCE(UNSETTLED_REFUND_SALE.DEPOSIT_VALUE,0)/100 AS deposit_value,
+  COALESCE(UNSETTLED_REFUND_SALE.PASS_COST,0)/100 AS pass_cost,
+  COALESCE(UNSETTLED_REFUND_SALE.TRANSIT_VALUE,0)/100 AS transit_value,
+  COALESCE(UNSETTLED_REFUND_SALE.BENEFIT_VALUE,0)/100 AS benefit_value,
+  COALESCE(UNSETTLED_REFUND_SALE.ORIG_SALE_AMT_PAID_BY_BV,0)/100 AS orig_sale_amt_paid_by_bv,
+  COALESCE(UNSETTLED_REFUND_SALE.ORIG_SALE_AMT_PAID_BY_NON_BV,0)/100 AS orig_sale_amt_paid_by_non_bv,
+  UNSETTLED_REFUND_SALE.ORDER_NBR,
+  UNSETTLED_REFUND_SALE.TRANSIT_ACCOUNT_ID,
+  UNSETTLED_REFUND_SALE.PURSE_NAME,
+  COALESCE(UNSETTLED_REFUND_SALE.RESTRICTED_PURSE_VALUE,0)/100 AS restricted_purse_value,
+  COALESCE(UNSETTLED_REFUND_SALE.REFUND_FEE/100,0) AS refund_fee,
+  UNSETTLED_REFUND_SALE.RETRIEVAL_REF_NBR,
+  COALESCE(UNSETTLED_REFUND_SALE.PREPAID_BANKCARD_VALUE/100,0) AS prepaid_bankcard_value,
+  UNSETTLED_REFUND_SALE.PAYMENT_TRANSIT_ACCOUNT_ID
+FROM
+  UNSETTLED_REFUND_SALE);
+"""
+
+WC232_USE_VIEW = """
+CREATE OR REPLACE VIEW ods.wc232_use AS (SELECT DISTINCT
+  euu.SETTLEMENT_STATE,
+  COALESCE(euu.TRANSIT_VALUE,0)/100 AS transit_value,
+  euu.TRANSIT_ACCOUNT_ID,
+  euu.PASS_ID,
+  eod.OPERATOR_NAME,
+  efpd.FARE_PROD_NAME,
+  euu.DEVICE_ID,
+  euu.SERIAL_NBR,
+  euu.PASS_USE_COUNT,
+  euu.POSTING_DAY_KEY,
+  COALESCE(euu.BENEFIT_VALUE,0)/100 AS benefit_value,
+  COALESCE(euu.BANKCARD_PAYMENT_VALUE,0)/100 AS bankcard_payment_value,
+  COALESCE(euu.PASS_COST,0)/100 AS pass_cost,
+  euu.READY_FOR_SETTLEMENT_DTM,
+  euu.TRANSACTION_DTM,
+  COALESCE(euu.ONE_ACCOUNT_VALUE,0)/100 AS one_account_amount,
+  COALESCE(euu.UNCOLLECTIBLE_AMOUNT,0)/100 AS uncollectible_amount,
+  eptd.PURSE_NAME,
+  COALESCE(euu.RESTRICTED_PURSE_VALUE,0)/100 AS restricted_purse_value,
+  COALESCE(euu.POST_PAY_AMOUNT/100,0) AS post_pay_amount,
+  euu.RETRIEVAL_REF_NBR,
+  COALESCE(euu.REFUNDABLE_PURSE_VALUE,0)/100 AS refundable_purse_value,
+  COALESCE(euu.PREPAID_BANKCARD_VALUE/100,0) AS prepaid_bankcard_value
+FROM ods.edw_unsettled_use euu
+	LEFT JOIN ods.edw_operator_dimension eod ON euu.operator_id = eod.operator_id
+	LEFT JOIN ods.edw_fare_product_dimension efpd ON euu.fare_prod_key = efpd.fare_prod_key
+	LEFT JOIN ods.edw_purse_type_dimension eptd ON euu.purse_sku = eptd.purse_sku
+	);
+"""
