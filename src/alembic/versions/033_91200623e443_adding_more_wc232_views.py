12458147099cb5becef2c50fb7d73b3a9c45e55c
Ted Alexander <talexander3@mbta.com>
2026-05-15 15:10:07 -0400

    Formatting

--- src/alembic/versions/033_91200623e443_adding_more_wc232_views.py
+++ src/alembic/versions/033_91200623e443_adding_more_wc232_views.py
@@ -11,11 +11,18 @@ from typing import Sequence, Union
 from alembic import op
 
 from cubic_loader.utils.postgres import DatabaseManager
-from cubic_loader.qlik.sql_strings.views import WC232_SYS_CONF, WC232_SALE, WC232_PAYMENT_REJECTION, WC232_MISC, WC232_DEVICE_CASH_STC, WC232_ACQ_CONF
+from cubic_loader.qlik.sql_strings.views import (
+    WC232_SYS_CONF,
+    WC232_SALE,
+    WC232_PAYMENT_REJECTION,
+    WC232_MISC,
+    WC232_DEVICE_CASH_STC,
+    WC232_ACQ_CONF,
+)
 
 # revision identifiers, used by Alembic.
-revision: str = '91200623e443'
-down_revision: Union[str, None] = '36f89551fe75'
+revision: str = "91200623e443"
+down_revision: Union[str, None] = "36f89551fe75"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -32,6 +39,7 @@ def upgrade() -> None:
     op.execute(WC232_MISC)
     op.execute(WC232_DEVICE_CASH_STC)
     op.execute(WC232_ACQ_CONF)
+
 
 def downgrade() -> None:
     op.execute("DROP VIEW IF EXISTS cubic_reports.wc232_sys_conf;")
